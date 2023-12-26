ea5d18664014ca40626028534ef2ea2902dcc7d4
omar khattab <omar.khattab7@icloud.com>
2023-12-26 17:51:46 +0200

    Initial commit

---
+++ Queue/QueueArray.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class QueueArray
+    {
+        int Front, Rear;
+        int[] Data;
+        public QueueArray(int size)
+        {
+            Data = new int[size];
+            Front = Rear = -1;
+        }
+        bool IsEmpty()
+        {
+            if (Front == -1||Front == Rear++)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        bool IsFull()
+        {
+            if (Rear == Data.Length - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Enqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue over flow");
+                return;
+            }
+            if(Front == -1)
+            {
+                Front = 0;
+            }
+            Rear++;
+            Data[Rear] = item;
+        }
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("queue under flow");
+                return 0;
+            }
+            int x = Data[Front];
+            Front++;
+            return x;
+        }
+        public int PeekFront()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return -1;
+            }
+            return Data[Front];
+        }
+        public int PeekEnd()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return -1;
+            }
+            return Data[Rear];
+        }
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            for(int i = Front; i <= Rear; i++)
+            {
+                Console.Write($"{Data[i]}  ");
+            }
+        }
+    }
+}
---
+++ Queue/QueueList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class QueueList
+    {
+        class Node
+        {
+            public int Data;
+            public Node Next;
+        }
+        Node Front, Rear;
+        public QueueList() {
+            Front = Rear = null;
+        }
+        public bool IsEmpty()
+        {
+            if (Front == null) return true;
+            else return false;
+        }
+        public void EnQueue(int Value)
+        {
+            Node temp = new Node();
+            temp.Data = Value;
+            if (IsEmpty())
+            {
+                Front = temp;
+            }
+            else
+            {
+                Rear.Next = temp;
+            }
+            Rear = temp;
+        }
+        public int DeQueue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue under flow");
+                return int.MinValue;
+            }
+            int Value = Front.Data;
+            Front = Front.Next;
+            return Value;
+        }
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return int.MinValue;
+            }
+            return Front.Data;
+        }
+        public void Display()
+        {
+            Node p = Front;
+            while( p != null )
+            {
+                Console.Write($"{p.Data}  ");
+                p = p.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
